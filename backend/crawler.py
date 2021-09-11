d6f990d191234a6cf04159b69b57202a630fcd7b
yangbaechu <45089264+yangbaechu@users.noreply.github.com>
2021-09-11 07:43:26 +0000

    python 환경 구성

--- backend/crawler.py
+++ backend/crawler.py
@@ -4,7 +4,9 @@ sys.path.append('/usr/lib/python3/dist-packages')
 from urllib.parse import urlencode, unquote
 import requests
 import json
+import pandas as pd
 
+#SSL: DH_KEY_TOO_SMALL 오류 해결
 requests.packages.urllib3.disable_warnings()
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
 try:
@@ -13,23 +15,12 @@ except AttributeError:
     # no pyopenssl support used / needed / available
     pass
 
-url = "https://openapi.mnd.go.kr/3935313630333536343431323332303830/json/DS_TB_MNDT_DATEBYMLSVC_8623/11/12/"
-queryString = "?" + urlencode(
-{
-  "Key": 3935313630333536343431323332303830,
-  "TYPE": "json",
-  "SERVICE": "DS_TB_MNDT_DATEBYMLSVC_8623",
-  "START_INDEX": 0,
-  "END_INDEX": 5
-}
-)
-queryURL = url + queryString
-response = requests.get(queryURL)
-jsonObject = json.loads(response.text)
-print(jsonObject.get("DS_TB_MNDT_DATEBYMLSVC_8623").get("row")[1].get("lunc"))
-"""
-print("=== response json data start ===")
-print(response.text)
-print("=== response json data end ===")
-print()
-"""
+#데이터 크롤링하고 csv 파일 형태로 저장
+url = "https://openapi.mnd.go.kr/3935313630333536343431323332303830/json/DS_TB_MNDT_DATEBYMLSVC_8623/1/100/"
+
+response = requests.get(url)
+jsonObject = json.loads(response.text).get('DS_TB_MNDT_DATEBYMLSVC_8623').get('row')
+print(jsonObject)
+df = pd.json_normalize(jsonObject)
+print(df.sort_values(by='dates')[:10])
+df.to_csv("samplecsv.csv")
