6ebc9c3bd7020449617980d8ed7d735824520842
yangbaechu <45089264+yangbaechu@users.noreply.github.com>
2021-09-27 14:50:14 +0000

    Dockerfile 삭제

--- backend/crawler.py
+++ backend/crawler.py
@@ -1,10 +1,10 @@
 import sys
 sys.path.append('/usr/lib/python3/dist-packages')
 
-from urllib.parse import urlencode, unquote
 import requests
 import json
 import pandas as pd
+import schedule
 
 #SSL: DH_KEY_TOO_SMALL 오류 해결
 requests.packages.urllib3.disable_warnings()
@@ -15,19 +15,28 @@ except AttributeError:
     # no pyopenssl support used / needed / available
     pass
 
-#각종 인자 설정
-troop_code = 9030
+def crawler():
 
-#데이터 크롤링하고 parsing
-url = "https://openapi.mnd.go.kr/3935313630333536343431323332303830/json/DS_TB_MNDT_DATEBYMLSVC_" + str(troop_code) + "/1/100/"
+    troop_code_list = [8623, 9030, 6282, 1691, 2171, 7369, 3296, 5322, 6335, 3389, 8902, 6176]
+    
+    #각 부대 식단표 수집
+    for troop_code in troop_code_list:
 
-response = requests.get(url)
-service_name = 'DS_TB_MNDT_DATEBYMLSVC_' + str(troop_code)
-jsonObject = json.loads(response.text).get(service_name).get('row')
-print(jsonObject)
+        #데이터 크롤링하고 parsing
+        url = "https://openapi.mnd.go.kr/3935313630333536343431323332303830/json/DS_TB_MNDT_DATEBYMLSVC_" + str(troop_code) + "/1/100/"
 
-#크롤링한 데이터 dataframe 형태로 저장
-df = pd.json_normalize(jsonObject)
-df.insert(0, "troop_code", 8623)
-df = df.sort_values(by='dates')
-df.to_csv("samplecsv.csv")
+        response = requests.get(url)
+        service_name = 'DS_TB_MNDT_DATEBYMLSVC_' + str(troop_code)
+        jsonObject = json.loads(response.text).get(service_name).get('row')
+        print(jsonObject)
+
+        #크롤링한 데이터 dataframe 형태로 저장
+        df = pd.json_normalize(jsonObject)
+        df.insert(0, "troop_code", troop_code)
+        df = df.sort_values(by='dates')
+        file_name = str(troop_code) + " menus.csv"
+        df.to_csv(file_name)
+
+
+crawler()
+schedule.every().day.at("22:30").do(crawler())
