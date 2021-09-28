09fc53e3521e2223d805caa8c01f8570b26af67a
star8041 <58851365+star8041@users.noreply.github.com>
2021-09-28 13:10:14 +0000

    앱 실행 시 첫 화면 구현 완료

--- APP/myapp/lib/main.dart
+++ APP/myapp/lib/main.dart
@@ -1,20 +1,97 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(disFir());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: const Text('Hello World'),
-        ),
-      ),
-    );
-  }
+class disFir extends StatelessWidget {
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'HelpMeal',
+            home: Scaffold(
+                body: Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget> [
+                            Icon(
+                                Icons.local_restaurant,
+                                size: 200,
+                            ),
+                            Text.rich(
+                                TextSpan(
+                                    text: '',
+                                    children: <TextSpan> [
+                                        TextSpan(
+                                            text: 'H',
+                                            style: TextStyle(
+                                                fontSize: 80.0,
+                                                color: Colors.red.shade400,
+                                                fontWeight: FontWeight.bold,
+                                            )
+                                        ),
+                                        TextSpan(
+                                            text: 'elp',
+                                            style: TextStyle(
+                                                fontSize: 80.0,
+                                                fontWeight: FontWeight.bold,
+                                            )
+                                        )
+                                    ]
+                                )
+                            ),
+                            Text.rich(
+                                TextSpan(
+                                    text: '',
+                                    children: <TextSpan> [
+                                        TextSpan(
+                                            text: 'M',
+                                            style: TextStyle(
+                                                fontSize: 80.0,
+                                                color: Colors.amber,
+                                                fontWeight: FontWeight.bold,
+                                            )
+                                        ),
+                                        TextSpan(
+                                            text: 'eal',
+                                            style: TextStyle(
+                                                fontSize: 80.0,
+                                                fontWeight: FontWeight.bold,
+                                            )
+                                        )
+                                    ]
+                                )
+                            ),
+                            SizedBox(
+                                height: 100,
+                            ),
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget> [
+                                    ButtonTheme(
+                                        minWidth: 200,
+                                        height: 50,
+                                        child: RaisedButton(
+                                            child: Text('로그인',style: TextStyle(fontSize: 20)),
+                                            onPressed: () {},
+                                            color: Colors.blue,
+                                            textColor: Colors.white,
+                                        ),
+                                    ),
+                                    ButtonTheme(
+                                        minWidth: 200,
+                                        height: 50,
+                                        child: RaisedButton(
+                                            child: Text('로그인 없이 시작',style: TextStyle(fontSize: 20)),
+                                            onPressed: () {},
+                                            color: Colors.blue,
+                                            textColor: Colors.white,
+                                        ),
+                                    ),
+                                ]
+                            )
+                        ]
+                    )
+                )
+            ),
+        );
+    }
 }
