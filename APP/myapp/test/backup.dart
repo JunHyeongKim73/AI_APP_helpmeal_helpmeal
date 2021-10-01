f2f0cc51e75850baab84197b37c55408c57c46cf
star8041 <58851365+star8041@users.noreply.github.com>
2021-10-01 10:58:46 +0000

    upload

---
+++ APP/myapp/test/backup.dart
@@ -0,0 +1,98 @@
+
+<<<<<<< HEAD
+void main() => runApp(disFir());
+
+class disFir extends StatelessWidget {
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'HelpMeal',
+            home: Scaffold(
+                body: Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget> [
+                            Icon(
+                                Icons.local_restaurant,
+                                size: 200,
+                            ),
+                            Text.rich(
+                                TextSpan(
+                                    text: '',
+                                    children: <TextSpan> [
+                                        TextSpan(
+                                            text: 'H',
+                                            style: TextStyle(
+                                                fontSize: 80.0,
+                                                color: Colors.red.shade400,
+                                                fontWeight: FontWeight.bold,
+                                            )
+                                        ),
+                                        TextSpan(
+                                            text: 'elp',
+                                            style: TextStyle(
+                                                fontSize: 80.0,
+                                                fontWeight: FontWeight.bold,
+                                            )
+                                        )
+                                    ]
+                                )
+                            ),
+                            Text.rich(
+                                TextSpan(
+                                    text: '',
+                                    children: <TextSpan> [
+                                        TextSpan(
+                                            text: 'M',
+                                            style: TextStyle(
+                                                fontSize: 80.0,
+                                                color: Colors.amber,
+                                                fontWeight: FontWeight.bold,
+                                            )
+                                        ),
+                                        TextSpan(
+                                            text: 'eal',
+                                            style: TextStyle(
+                                                fontSize: 80.0,
+                                                fontWeight: FontWeight.bold,
+                                            )
+                                        )
+                                    ]
+                                )
+                            ),
+                            SizedBox(
+                                height: 100,
+                            ),
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget> [
+                                    ButtonTheme(
+                                        minWidth: 200,
+                                        height: 50,
+                                        child: RaisedButton(
+                                            child: Text('로그인',style: TextStyle(fontSize: 20)),
+                                            onPressed: () {},
+                                            color: Colors.blue,
+                                            textColor: Colors.white,
+                                        ),
+                                    ),
+                                    ButtonTheme(
+                                        minWidth: 200,
+                                        height: 50,
+                                        child: RaisedButton(
+                                            child: Text('로그인 없이 시작',style: TextStyle(fontSize: 20)),
+                                            onPressed: () {},
+                                            color: Colors.blue,
+                                            textColor: Colors.white,
+                                        ),
+                                    ),
+                                ]
+                            )
+                        ]
+                    )
+                )
+            ),
+        );
+    }
+}
+=======
