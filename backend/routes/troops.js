d89cf6a4387650fd41e5ecc14213e2b764092042
yangbaechu <45089264+yangbaechu@users.noreply.github.com>
2021-10-20 19:33:56 +0900

    Delete backend directory

--- backend/routes/troops.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersController = require('./controllers/troops.controllers');
-
-router.post('/post', usersController.addNewTroop);
-router.post('/get', usersController.searchTroop);
-
-module.exports = router;
-
